583e1b7f651bc53c7c87ca3e3678780e1457f1e2
Erik Bürger <erik.burger@hotmail.se>
2020-02-04 10:56:41 +0100

    creating table for test images and code to populate it

---
+++ sql/test_imagecreation_and_population.sql
@@ -0,0 +1,20 @@
+create table test_images(
+	image_id int not null, 
+	image_url varchar(50) not null, 
+    image_name varchar(50) not null,
+    primary key (image_id)); 
+    
+INSERT INTO test_images (image_id, image_url, image_name)
+VALUES("0","../test_images/0.glas.jpg", "glas");
+
+INSERT INTO test_images (image_id, image_url, image_name)
+VALUES("1","../test_images/1.pencil.jpg", "Pencil");
+
+INSERT INTO test_images (image_id, image_url, image_name)
+VALUES("2","../test_images/2.fork.jpg", "Fork");
+
+INSERT INTO test_images (image_id, image_url, image_name)
+VALUES("3","../test_images/3.spoon.jpg", "Spoon");
+
+INSERT INTO test_images (image_id, image_url, image_name)
+VALUES("4","../test_images/4.knife.jpg", "Knife");
