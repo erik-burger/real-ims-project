fdfa32ba51bca6a9367ed44c6caf9aa292c94ae4
alvaannett <alva.annett@gmail.com>
2020-02-05 08:48:54 +0100

    relative file paths and som other changes

--- html/patientdoctor.php
+++ html/patientdoctor.php
@@ -18,7 +18,7 @@
 <h1>Patient Profile</h1>
 
 <?php
-        include dirname(__DIR__).'\php\openDB.php';
+        include dirname(__DIR__).'\html\php\openDB.php';
         $result = mysqli_query($link,"select * 
         from patient
         where patient_id = 1")   
@@ -53,7 +53,7 @@
             echo '<h3>'."About".'</h3>';
             echo $desc.'<br/>';
         }
-        include dirname(__DIR__).'\php\closeDB.php';
+        include dirname(__DIR__).'\html\php\closeDB.php';
  ?>
 
 <h3>Statistics</h3>
