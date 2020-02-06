c1ea4575cee1968b552b5f9ee22cf8364b9abf2a
Erik Bürger <erik.burger@hotmail.se>
2020-02-06 13:11:41 +0100

    fixed the openDB name and added total_score in test_result

--- html/php/openDB.php
+++ html/php/openDB.php
@@ -1,9 +1,9 @@
 
 <?php
-$hostname = "localhost:3308"; 
-$username = "root"; 
-$password = ""; 
-$dbname   = "ims"; 
+$hostname = "back.db1.course.it.uu.se"; 
+$username = "spring20_ims_2"; 
+$password = "WwtPqL1R"; 
+$dbname   = "spring20_ims_2"; 
 $link = mysqli_connect($hostname, $username, $password, $dbname); 
  
 if (!$link) {   echo "Error: Unable to connect to MySQL." 
