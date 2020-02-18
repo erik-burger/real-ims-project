3e767d614e6c194f41bf2855ce602cfbba13e31c
alvaannett <alva.annett@gmail.com>
2020-02-18 18:36:03 +0100

    php anf html injections in chaneg information pages

--- webpage/general/openDB.php
+++ webpage/general/openDB.php
@@ -1,5 +1,35 @@
 
 <?php
+
+define('DB_SERVER', 'back.db1.course.it.uu.se:3306');
+define('DB_USERNAME', 'spring20_ims_2');
+define('DB_PASSWORD', 'WwtPqL1R');
+define('DB_NAME', 'spring20_ims_2');
+define('SERVER_IP', 'beurling.it.uu.se');
+define('username', 'alan4036');
+define('password', 'R13k5t1n5P13k5t1n5');
+
+
+$connection = ssh2_connect(SERVER_IP, 22);
+
+ssh2_auth_password($connection, username, password);
+
+$tunnel = ssh2_tunnel($connection, 'back.db1.course.it.uu.se', 3306);
+
+
+
+$link = mysqli_connect('127.0.0.1', DB_USERNAME, DB_PASSWORD, 
+                         DB_NAME, 3306, $tunnel);
+
+if (!$link) {
+	echo "Error: Unable to connect to MySQL." . mysqli_connect_error() . PHP_EOL;
+	exit;
+}
+?>
+
+
+<?php
+/*
 $hostname = "back.db1.course.it.uu.se:3306"; 
 $username = "spring20_ims_2"; 
 $password = "WwtPqL1R"; 
@@ -8,5 +38,5 @@ $link = mysqli_connect($hostname, $username, $password, $dbname);
  
 if (!$link) {   echo "Error: Unable to connect to MySQL." 
 . mysqli_connect_error() . PHP_EOL;  exit; } 
+*/
 ?>
-
--- webpage/researcher/update_researcher.php
+++ webpage/researcher/update_researcher.php
@@ -4,26 +4,47 @@ session_start();
 $id = $_SESSION["id"];
 include dirname(__DIR__).'/general/openDB.php';
 
+
 $f_name = $_POST["f_name"]; 
 $f_name = mysqli_real_escape_string($link, $f_name);
+$f_name = strip_tags($f_name); 
+
 $m_name = $_POST["m_name"];  
 $m_name = mysqli_real_escape_string($link, $m_name);
+$m_name = strip_tags($m_name); 
+
 $l_name = $_POST["l_name"];
 $l_name = mysqli_real_escape_string($link, $l_name);
+$l_name = strip_tags($l_name); 
+
 $phone_no = $_POST["phone_no"]; 
 $phone_no = mysqli_real_escape_string($link, $phone_no);
+$f_name = strip_tags($f_name); 
+
 $street = $_POST["street"];
 $street = mysqli_real_escape_string($link, $street);
+$street = strip_tags($street); 
+
 $street_no = $_POST["street_no"];
 $street_no = mysqli_real_escape_string($link, $street_no);
+$street_no = strip_tags($street_no); 
+
 $city = $_POST["city"];
 $city = mysqli_real_escape_string($link, $city);
+$city = strip_tags($city); 
+
 $country = $_POST["country"];
 $country = mysqli_real_escape_string($link, $country);
+$country = strip_tags($country); 
+
 $zip = $_POST["zip"];
 $zip = mysqli_real_escape_string($link, $zip);
+$zip = strip_tags($zip); 
+
 $email = $_POST["email"];
 $email = mysqli_real_escape_string($link, $email);
+$email = strip_tags($email); 
+
 $psw = $_POST["psw"];
 
 //get the hashed password
