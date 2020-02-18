5a06eac10fa25de06fdf9a01262b2819cd88a62d
alvaannett <alva.annett@gmail.com>
2020-02-18 20:55:51 +0100

    fixed same type of data type checks as in the registration forms for the update information forms

--- webpage/general/openDB.php
+++ webpage/general/openDB.php
@@ -1,6 +1,6 @@
 
 <?php
-
+/*
 define('DB_SERVER', 'back.db1.course.it.uu.se:3306');
 define('DB_USERNAME', 'spring20_ims_2');
 define('DB_PASSWORD', 'WwtPqL1R');
@@ -9,14 +9,11 @@ define('SERVER_IP', 'beurling.it.uu.se');
 define('username', 'alan4036');
 define('password', 'R13k5t1n5P13k5t1n5');
 
-
 $connection = ssh2_connect(SERVER_IP, 22);
 
 ssh2_auth_password($connection, username, password);
 
 $tunnel = ssh2_tunnel($connection, 'back.db1.course.it.uu.se', 3306);
-
-
 
 $link = mysqli_connect('127.0.0.1', DB_USERNAME, DB_PASSWORD, 
                          DB_NAME, 3306, $tunnel);
@@ -25,11 +22,12 @@ if (!$link) {
 	echo "Error: Unable to connect to MySQL." . mysqli_connect_error() . PHP_EOL;
 	exit;
 }
+*/ 
 ?>
 
 
 <?php
-/*
+
 $hostname = "back.db1.course.it.uu.se:3306"; 
 $username = "spring20_ims_2"; 
 $password = "WwtPqL1R"; 
@@ -38,5 +36,5 @@ $link = mysqli_connect($hostname, $username, $password, $dbname);
  
 if (!$link) {   echo "Error: Unable to connect to MySQL." 
 . mysqli_connect_error() . PHP_EOL;  exit; } 
-*/
+
 ?>
