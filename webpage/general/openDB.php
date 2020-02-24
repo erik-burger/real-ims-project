7ed8734f9bf6784717c248f3de0ba7b1e7766c11
rpensch <rar.pensch@gmail.com>
2020-02-24 09:52:09 +0100

    JJJ

--- webpage/general/openDB.php
+++ webpage/general/openDB.php
@@ -1,9 +1,9 @@
 <?php
 	
-    $hostname = "localhost:3306"; //"back.db1.course.it.uu.se:330"; 
-    $username = "root"; //"spring20_ims_2"; 
-    $password = ""; //"WwtPqL1R"; 
-    $dbname   = "ims_out"; //"spring20_ims_2"; 
+    $hostname = "back.db1.course.it.uu.se:3306"; //"localhost:3306"; 
+    $username = "spring20_ims_2"; //"root"; 
+    $password = "WwtPqL1R"; //""; 
+    $dbname   = "spring20_ims_2"; //"ims_out"; 
 $link = mysqli_connect($hostname, $username, $password, $dbname); 
  
 if (!$link) {   echo "Error: Unable to connect to MySQL." 
