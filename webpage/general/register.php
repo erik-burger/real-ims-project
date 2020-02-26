200d30f32d9e303a53b8bdce62fe7bb8a9ccf516
alvaannett <alva.annett@gmail.com>
2020-02-26 14:53:14 +0100

    merge registration pages

---
+++ webpage/general/register.php
@@ -0,0 +1,34 @@
+<html>
+
+<?php
+
+if (isset($logedin) or isset($user)) {
+    if ($logedin == 1) {
+        switch ($user) {
+            case 'D':
+                ?>
+                <!--Code-->
+                <?php
+                break;
+            case 'P':
+                ?>
+                <!--Code-->
+                <?php
+                break;
+            case 'C':
+                ?>
+                <!--Code-->
+                <?php
+                break;
+            case 'R':
+                ?>
+                <!--Code-->
+                <?php
+                break;
+        }
+    } else {
+    echo "<script>window.location.href = '../general/login.php';</script>";
+    }
+}else {
+echo "<script>window.location.href = '../general/login.php';</script>";
+}
