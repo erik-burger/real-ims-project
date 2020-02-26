dcb2986a533ead3c8ac388c3d8c3421774701a83
rpensch <rar.pensch@gmail.com>
2020-02-26 11:37:15 +0100

    pasw

--- webpage/doctor/doctor_registration.php
+++ webpage/doctor/doctor_registration.php
@@ -133,7 +133,7 @@ if(isset($_POST["submit"])){
 		$errors['psw'] = 'Please repeat your password';
 	}elseif($_POST['psw']!= $_POST['psw_repeat']){
 		$errors['psw'] = "Passwords do not match, pleas try again!";
-	}elseif(!$uppercase || !$lowercase || !$number || strlen($password) < 8){
+	}elseif($uppercase != 0|| $lowercase != 0 || $number != 0 || strlen($password) < 8){
 		$errors['psw'] = "Password should be at least 8 characters in length and should include at least one upper and lower case letter as well as one number.";
 	}else{
 		$psw = $link->real_escape_string($_POST['psw']);
