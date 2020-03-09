b016261531066e8904a866cc852a96539d6f2fb7
rpensch <rar.pensch@gmail.com>
2020-03-09 10:10:35 +0100

    KK

--- webpage/general/profile_page.php
+++ webpage/general/profile_page.php
@@ -130,7 +130,7 @@ if (isset($logedin) or isset($user)) {
                         from doctor
                         where doctor_id = $_SESSION[id]")   
                         or 
-                        die("Could not issue MySQL query"); 
+                        die("Could not issue huh MySQL query"); 
                         
                         while ($row = $result->fetch_assoc()) {
                             $first_name = $row["first_name"];
